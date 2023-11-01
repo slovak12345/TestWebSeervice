4e6b80e5fc15a0ab68ee7ca4cdf0eb9c56c615f3
vicislave <slave@altione.altione.net>
2023-11-02 01:05:31 +0300

    addwq

--- Docker/webservice/docker-entrypoint.sh
+++ Docker/webservice/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /bin/bash
-
+sudo mongod &
 fluent-bit -c /usr/local/etc/fluent-bit/fluent-bit.conf &
 sudo -u opensearch /opt/opensearch/opensearch-tar-install.sh -Ecluster.name=opensearch-cluster -Enode.name=opensearch-node1 -Ehttp.host=0.0.0.0 -Ediscovery.type=single-node &
 sudo -u opensearch /opt/opensearch-dashboards/bin/opensearch-dashboards 
--- src/python-project-template/project_name/cli.py
+++ src/python-project-template/project_name/cli.py
@@ -8,8 +8,75 @@ Be creative! do whatever you want!
 - Import things from your .base module
 """
 
+from aiohttp import web
+import json
+import threading
+from currentStatusWorker import CurrentStatusWorker
+from shutdownWorker import ShutdownWorker
+from workerStatusWorker import WorkerStatusWorker
+
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return web.Response(text=text)
+
+async def wshandle(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:
+        if msg.type == web.WSMsgType.text:
+            await ws.send_str("Hello, {}".format(msg.data))
+        elif msg.type == web.WSMsgType.binary:
+            await ws.send_bytes(msg.data)
+        elif msg.type == web.WSMsgType.close:
+            break
+
+    return ws
+
+current_status_data = {}
+working_status_data = {}
+shutdown_work_data = {}
+current_config_data = {}
+
+async def current_status_handle(request):
+    data = current_status_data
+    result = json.dumps(data) 
+    return web.Response(text=result)
+
+async def working_status_handle(request):
+    data = working_status_data
+    result = json.dumps(data) 
+    return web.Response(text=result)
+
+async def shutdown_work_handle(request):
+    data = shutdown_work_data
+    result = json.dumps(data) 
+    return web.Response(text=result)
+
+async def current_config_handle(request):
+    data = current_config_data
+    result = json.dumps(data) 
+    return web.Response(text=result)
+
+app = web.Application()
+app.add_routes([web.get('/', handle),
+                web.get('/echo', wshandle)])
+
+app.add_routes([web.get('/', handle),
+                web.get('/current-status', current_status_handle),
+                web.get('/working-status', working_status_handle),
+                web.get('/shutdown-work', shutdown_work_handle),
+                web.get('/current-config', current_config_handle)])
 
 def main():  # pragma: no cover
+    currentStatusWorker=CurrentStatusWorker()
+    shutdownWorker=ShutdownWorker()
+    workerStatusWorker=WorkerStatusWorker()
+
+    currentStatusWorker.join()
+    shutdownWorker.join()
+    workerStatusWorker.join()
     """
     The main function executes on commands:
     `python -m project_name` and `$ project_name `.
@@ -25,4 +92,4 @@ def main():  # pragma: no cover
         * List all available tasks
         * Run an application (Flask, FastAPI, Django, etc.)
     """
-    print("This will do something")
+    web.run_app(app)
