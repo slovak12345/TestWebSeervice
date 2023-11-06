ce76013b2a98d44285dc2367dc0c11d018ca8ba1
vicislave <slave@altione.altione.net>
2023-11-06 20:25:13 +0300

    fixed mongodbstart

--- Docker/webservice/docker-entrypoint.sh
+++ Docker/webservice/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 /bin/bash
-sudo mongod &
+sudo /usr/bin/mongod --config /etc/mongod.conf &
 fluent-bit -c /usr/local/etc/fluent-bit/fluent-bit.conf &
 sudo -u opensearch /opt/opensearch/opensearch-tar-install.sh -Ecluster.name=opensearch-cluster -Enode.name=opensearch-node1 -Ehttp.host=0.0.0.0 -Ediscovery.type=single-node &
 sudo -u opensearch /opt/opensearch-dashboards/bin/opensearch-dashboards 
