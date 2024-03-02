5407add2f216ad0f0a2375a2a6d242f54f6b4afd
vicislave <slave@altione.altione.net>
2024-03-02 14:03:09 +0300

    asds

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,16 +8,16 @@ sudo iptables -A INPUT -i eth0 -p tcp -m tcp --dport 27017 -j DROP
 set -e
 
 sudo chown -R opensearch:opensearch /opt/gcs/opensearch-dashboards/config/
-sudo chown -R opensearch:opensearch /opt//gcs/opensearch/config/
+sudo chown -R opensearch:opensearch /opt/gcs/opensearch/config/
 sudo chown -R fluentbit:fluentbit /opt/gcs/fluent-bit
 sudo chown redis:root /opt/gcs/redis/redis.conf
-sudo chmod 600 /opt/redis/gcs/redis.conf
-sudo chown gcs:root /opt/gcs/secrets.yml
-sudo chmod 600 /opt/gcs/secrets.yml
+sudo chmod 600 /opt/gcs/redis/gcs/redis.conf
+sudo chown gcs:root /opt/gcs/secrets/redis_secrets.yml
+sudo chmod 600 /opt/gcs/secrets/redis_secrets.yml
 sudo chown mongodb:root /opt/gcs/mongod.conf
 sudo chmod 600 /opt/gcs/mongod.conf
 
-echo "user $(yq -r '.redis.login' /opt/gcs/secrets.yml) on ~* &* +@all #$(echo -n $(yq -r '.redis.passwd' /opt/gcs/secrets.yml) | sha256sum | head -c 64)" >> /opt/redis/redis.conf
+echo "user $(yq -r '.redis.login' /opt/gcs/secrets/redis_secrets.yml) on ~* &* +@all #$(echo -n $(yq -r '.redis.passwd' /opt/gcs/secrets/redis_secrets.yml) | sha256sum | head -c 64)" >> /opt/gcs/redis/redis.conf
 
 if [ -z "$@" ]; then
   exec /usr/local/bin/supervisord -c /opt/supervisord.conf --nodaemon
