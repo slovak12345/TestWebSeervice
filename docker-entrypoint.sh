53ffa5a454df25af3ea7069a5a0101e90bd11bfd
vicislave <slave@altione.altione.net>
2024-03-03 13:25:34 +0300

    asdas

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,15 +7,15 @@ sudo iptables -A INPUT -i eth0 -p tcp -m tcp --dport 27017 -j DROP
 
 set -e
 
-sudo chown -R opensearch:opensearch /opt/gcs/opensearch-dashboards/config/
-sudo chown -R opensearch:opensearch /opt/gcs/opensearch/config/
-sudo chown -R fluentbit:fluentbit /opt/gcs/fluent-bit
-sudo chown redis:root /opt/gcs/redis/redis.conf
-sudo chmod 600 /opt/gcs/redis/redis.conf
-sudo chown gcs:root /opt/gcs/secrets/redis_secrets.yml
-sudo chmod 600 /opt/gcs/secrets/redis_secrets.yml
-sudo chown mongodb:root /opt/gcs/mongodb/mongod.conf
-sudo chmod 600 /opt/gcs/mongodb/mongod.conf
+# sudo chown -R opensearch:opensearch /opt/gcs/opensearch-dashboards/config/
+# sudo chown -R opensearch:opensearch /opt/gcs/opensearch/config/
+# sudo chown -R fluentbit:fluentbit /opt/gcs/fluent-bit
+# sudo chown redis:root /opt/gcs/redis/redis.conf
+# sudo chmod 600 /opt/gcs/redis/redis.conf
+# sudo chown gcs:root /opt/gcs/secrets/redis_secrets.yml
+# sudo chmod 600 /opt/gcs/secrets/redis_secrets.yml
+# sudo chown mongodb:root /opt/gcs/mongodb/mongod.conf
+# sudo chmod 600 /opt/gcs/mongodb/mongod.conf
 
 redis_user_login=$(yq -r '.redis.login' /opt/gcs/secrets/redis_secrets.yml)
 redis_user_passwd=$(echo -n $(yq -r '.redis.passwd' /opt/gcs/secrets/redis_secrets.yml) | sha256sum | head -c 64)
