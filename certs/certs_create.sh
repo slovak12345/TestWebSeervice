c33ee6bbe2b8c3016e7c942d57e6c9b43e0a825e
Slava Tsoy <tsoj.slava2017@yandex.ru>
2024-03-06 21:48:57 +0300

    asdas

---
+++ certs/certs_create.sh
@@ -0,0 +1,14 @@
+
+#Create Certificate Authority (CA) Certificate and Key:
+openssl genrsa -out ca-key.pem 4096
+openssl req -new -x509 -sha256 -key ca-key.pem -out ca-cert.pem -days 365
+
+#Create Server Certificate and Key:
+openssl genrsa -out server-key.pem 4096
+openssl req -new -key server-key.pem -out server.csr
+openssl x509 -req -sha256 -in server.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out server-cert.pem -days 365
+
+#Create Client Certificate and Key (Optional):
+openssl genrsa -out client-key.pem 4096
+openssl req -new -key client-key.pem -out client.csr
+openssl x509 -req -sha256 -in client.csr -CA ca-cert.pem -CAkey ca-key.pem -CAcreateserial -out client-cert.pem -days 365
