225cf10430b222811ddda373344ea2f4bba820eb
Slava Tsoy <tsoj.slava2017@yandex.ru>
2024-03-10 20:12:47 +0300

    2

--- opt/mongodb/create_users.js
+++ opt/mongodb/create_users.js
@@ -1,6 +1,6 @@
 db.createUser({
-    user: "gcs-mongodb",
-    pwd: "onsdfs",
+    user: "myUser",
+    pwd: "myPassowrd",
     roles: [
       { role: "readWrite", db: "my_database" },
       { role: "read", db: "another_database" }
