f41c58af66d93e86430cd1f4639f98e77058906e
Slava Tsoy <tsoj.slava2017@yandex.ru>
2024-03-10 20:33:22 +0300

    asd

--- opt/mongodb/create_users.js
+++ opt/mongodb/create_users.js
@@ -2,8 +2,7 @@ db.createUser({
     user: "myUser",
     pwd: "myPassword",
     roles: [
-      { role: "readWrite", db: "my_database" },
-      { role: "read", db: "another_database" }
+      { role: "readWrite", db: "mydatabase" }
     ]
   })
   
